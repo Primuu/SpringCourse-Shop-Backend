9ace060e7ea31c7d8db5b58e4b24b5d6d9f9dd55
Adam <trentowski.adam@gmail.com>
2023-06-17 13:25:58 +0200

    Added simple getProducts from db

--- src/main/java/pl/nullpointerexception/shop/product/controller/ProductController.java
+++ src/main/java/pl/nullpointerexception/shop/product/controller/ProductController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.nullpointerexception.shop.product.model.Product;
+import pl.nullpointerexception.shop.product.service.ProductService;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -14,11 +14,11 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
 
+    private final ProductService productService;
+
     @GetMapping("")
     public List<Product> getProducts() {
-        return List.of(
-                new Product("Name 1", "Category 1", "Desc", new BigDecimal("8.99"), "PLN")
-        );
+        return productService.getProducts();
     }
 
 }
--- src/main/java/pl/nullpointerexception/shop/product/model/Product.java
+++ src/main/java/pl/nullpointerexception/shop/product/model/Product.java
@@ -1,14 +1,26 @@
 package pl.nullpointerexception.shop.product.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+@Entity
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+// Identity e.g. in MySQL, Sequence in Postgres
 
     private String name;
     private String category;
